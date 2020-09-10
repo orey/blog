621a8aac885200e6e406d3c079d7ccf507d384a7
Olivier Rey <rey.olivier@gmail.com>
2020-09-10 15:21:48 +0200

    $1

---
+++ publish.bat
@@ -0,0 +1,20 @@
+@echo off
+
+if [%1]==[] goto usage
+   ..\setenv setenv-python-3.8.3.bat
+   @echo "======================= git add *"
+   git add *
+   @echo "======================= git commit -a -m " $1
+   git commit -a -m "$1"
+   @echo "======================= git push origin master"
+   git push origin master
+   @echo "======================= mkdocs gh-deploy"
+   mkdocs gh-deploy
+   @echo "Done"
+goto :eof
+
+:usage
+echo Usage: C:\path\publish.bat COMMENT_BETWEEN_QUOTES
+exit /B 1
+
+:eof
