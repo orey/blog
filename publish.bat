cb954357d465e421764449c7606eef42afbb4116
Olivier Rey <rey.olivier@gmail.com>
2020-09-10 15:30:31 +0200

    nouvelle version du batch

--- publish.bat
+++ publish.bat
@@ -2,15 +2,15 @@
 
 if [%1]==[] goto usage
    call ..\setenv-python-3.8.3.bat
-   @echo "======================= git add *"
+   @echo ======================= git add *
    git add *
-   @echo "======================= git commit -a -m " $1
-   git commit -a -m "$1"
-   @echo "======================= git push origin master"
+   @echo ======================= git commit -a -m COMMENT
+   git commit -a -m %1
+   @echo ======================= git push origin master
    git push origin master
-   @echo "======================= mkdocs gh-deploy"
+   @echo ======================= mkdocs gh-deploy
    mkdocs gh-deploy
-   @echo "Done"
+   @echo Done
 goto :eof
 
 :usage
